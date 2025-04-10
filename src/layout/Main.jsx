268489f8b068546b5cb6152495b1d5888a205f87
Xian Saiful <xiansaiful@gmail.com>
2025-04-10 22:55:20 +0600

    Setup basic environment for photosHUB application

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import Nav from "../layout/Nav"
+import Footer from "../layout/Footer"
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
