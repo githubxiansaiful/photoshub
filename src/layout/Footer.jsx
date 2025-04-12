7e1da42d2848ae168fff7ab4ceafc9b18c1f4aae
Xian Saiful <xiansaiful@gmail.com>
2025-04-12 23:29:43 +0600

    Added Google, Faceboo, Twitter Login Buttons

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="bg-gray-900 text-gray-300">
             <div className="container">
                 <footer className="">
-                    <div className="max-w-7xl mx-auto  sm:px-6 lg:px-8 py-12">
+                    <div className="max-w-7xl mx-auto py-12">
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                             {/* Column 1: About Website */}
                             <div>
--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -7,7 +7,20 @@ const Login = () => {
                 <div>
                     <div className='auth-content-area'>
                         <h1 className='text-3xl lg:text-4xl font-bold text-center'>Welcome Back 👋</h1>
-                        <div>Providers</div>
+                        <div className='login-buttons mt-5 mb-5'>
+                            <button>
+                                <img src="/google.svg" />
+                                <span className="text-[16px] font-medium">Log in with Google</span>
+                            </button>
+                            <button>
+                                <img src="/facebook.svg" />
+                                <span className="text-[16px] font-medium">Log in with Facebook</span>
+                            </button>
+                            <button>
+                                <img src="/twitter.svg" />
+                                <span className="text-[16px] font-medium">Log in with X(Twitter)</span>
+                            </button>
+                        </div>
                         <div className='or-login-with-email'>
                             <p>Or, log in with your email</p>
                         </div>
