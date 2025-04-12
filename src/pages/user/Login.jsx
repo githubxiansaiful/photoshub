ca742038599dfa2c23cfdfd2e7dd9bf4d10f1ca5
Xian Saiful <xiansaiful@gmail.com>
2025-04-12 21:16:52 +0600

    Added login page

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='auth-page'>
+            <div className="container">
+                <div>
+                    <div className='auth-content-area'>
+                        <h1 className='text-4xl font-bold text-center'>Welcome Back 👋</h1>
+                        <div>Providers</div>
+                        <div className='or-login-with-email'>
+                            <p>Or, log in with your email</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
